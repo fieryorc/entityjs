82506f19d073684892235d66b8d865bf2a260792
Prem Ramanathan <premanandr@gmail.com>
2016-07-17 00:26:36 -0700

    Add sample, comments.

---
+++ samples/employee.ts
@@ -0,0 +1,133 @@
+import { CloudStoreEntity, ValueProperty, ReferenceProperty, PrimaryKeyProperty, DataContext, CloudDataStore} from "../main";
+import * as Promise from "bluebird";
+
+/*
+ * This sample provides a simple demonstration of using the StorageEntities in entityjs.
+ */
+
+class EmployeeEntity extends CloudStoreEntity {
+
+    public static KIND = "user";
+
+    /**
+     * This means the property is a primary key.
+     * Every CloudStoreEntity should have one and only primary key.
+     */
+    @PrimaryKeyProperty()
+    public id: string;
+
+    /**
+     * This means the property is a value property.
+     */
+    @ValueProperty()
+    public name: string;
+
+    /**
+     * Represents reference property.
+     * Reference properties are not loaded by default. You need to load explicity using
+     * .load() on them.
+     */
+    @ReferenceProperty(EmployeeEntity)
+    public manager: EmployeeEntity;
+
+    public constructor() {
+        super(EmployeeEntity.KIND);
+    }
+}
+
+
+class EmployeeHelper {
+    private context: DataContext;
+
+    public constructor(context: DataContext) {
+        this.context = context;
+    }
+
+    /**
+     * Adds a new employee to the database. If employee already exists,
+     * fails.
+     */
+    public addEmployee(id: string, name: string): Promise<EmployeeEntity> {
+        var employee = new EmployeeEntity();
+        employee.setContext(this.context);
+        employee.id = id;
+        return employee.load()
+            .then((isLoaded: boolean) => {
+                if (isLoaded) {
+                    throw `Employee with id(${id}) already exists.`;
+                }
+                employee.name = name;
+                return employee.save();
+            })
+            .then(() => {
+                return employee;
+            });
+    }
+
+    /**
+     * Set manager for an employee.
+     */
+    public setManager(id: string, managerId: string): Promise<void> {
+        var employee = new EmployeeEntity();
+        employee.setContext(this.context);
+        employee.id = id;
+
+        var manager = new EmployeeEntity();
+        manager.setContext(this.context);
+        manager.id = managerId;
+
+        return employee.load()
+            .then(isLoaded => {
+                if (!isLoaded) {
+                    throw `Employee id(${id} doesn't exist.`;
+                }
+                return manager.load();
+            })
+            .then(isManagerLoaded => {
+                if (!isManagerLoaded) {
+                    throw `Manager id(${managerId} doesn't exist.`;
+                }
+                employee.manager = manager;
+                return employee.save();
+            });
+    }
+
+    /**
+     * Removes the employee from the database.
+     */
+    public removeEmployee(id: string): Promise<void> {
+        var employee = new EmployeeEntity();
+        employee.setContext(this.context);
+        employee.id = id;
+
+        return employee.delete();
+    }
+
+    /**
+     * Gets the employee from the database.
+     */
+    public getEmployee(id: string): Promise<EmployeeEntity> {
+        var employee = new EmployeeEntity();
+        employee.setContext(this.context);
+        employee.id = id;
+
+        return employee.load()
+            .then(isLoaded => employee);
+    }
+}
+
+var helper = new EmployeeHelper(new DataContext(new CloudDataStore("service-backend-sample")));
+helper
+    .addEmployee("prem", "Prem Ramanathan")
+    .then(() => {
+        return helper.addEmployee("prem-boss", "Prem Boss");
+    })
+    .then(() => {
+        return helper.setManager("prem", "prem-boss");
+    })
+    .then(() => {
+        console.log("All completed successfully");
+    })
+    .catch(err => {
+        console.error(`Failed ${err}`);
+    });
